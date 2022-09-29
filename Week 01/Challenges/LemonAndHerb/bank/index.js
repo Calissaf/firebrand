4010fa8826a3007fddb32586f69b3177f8848afd
Calissaf <calissa@praxitar.com>
2022-09-29 18:00:53 +0100

    Added authorization to bank

--- Week 01/Challenges/LemonAndHerb/bank/index.js
+++ Week 01/Challenges/LemonAndHerb/bank/index.js
@@ -1,9 +1,14 @@
-function checkPin(userPin) {
+function checkPin() {
+    if (authorized) {
+        return true;
+    }
+
     let inputPin = parseInt(prompt("Enter pin: "), 10)
     let triesAllowed = 3;
 
     for (let i = triesAllowed; i > 0; i--) {
         if (inputPin == userPin) {
+            authorized = true;
             return true;
         } else {
             inputPin = parseInt(prompt((`Invalid pin ${i} tries remaining\n Enter pin: `)), 10)
@@ -13,51 +18,48 @@ function checkPin(userPin) {
 }
 
 
-function viewBalance(userbalance) {
-    alert(`Your balance is: £ ${userbalance}`);
-    console.log(`Your balance is: £ ${userbalance}`);
+function viewBalance() {
+    console.log(`Your balance is: £ ${userBalance}`);
 }
 
-function alterBalance(userBalance, changeAmmount) {
+function alterBalance(changeAmmount) {
     let newBalance = userBalance + changeAmmount;
     if (newBalance > 0){
-        alert("Transaction successful");
         console.log("Transaction successful");
         return newBalance;
     } else {
-        alert("Error please try again");
-        console.log("Error please try again");
-        repeatOptions();
+        console.log("Error transaction unsuccessfull");
+        return userBalance;
     }
 }
 
-function depositMoney(userBalance) {
+function depositMoney() {
     let despositAmmount = parseInt(prompt("How much would you like to deposit?"), 10);
-    return alterBalance(userBalance, Math.abs(despositAmmount));
+    return alterBalance(Math.abs(despositAmmount));
 }
 
-function withdrawMoney(userBalance) {
+function withdrawMoney() {
     let withdrawalAmmount = parseInt(prompt("How much would you like to withdraw?"), 10);
     withdrawalAmmount = Math.abs(withdrawalAmmount) * -1;
-    return alterBalance(userBalance, withdrawalAmmount);
+    return alterBalance(withdrawalAmmount);
 }
 
 
 function options() {
     let userInput = prompt("Please select an option:\n 1.View balance\n 2.Deposit money\n 3.Withdraw money\n 4.Exit");
     
-    if (userInput == "1" && checkPin(userPin) == true) {
-        viewBalance(userBalance);
+    if (userInput == "1" && checkPin()) {
+        viewBalance();
         repeatOptions();
-    } else if (userInput == "2" && checkPin(userPin) == true) {
-        userBalance = depositMoney(userBalance);
+    } else if (userInput == "2" && checkPin()) {
+        userBalance = depositMoney();
         repeatOptions();
-    } else if (userInput == "3" && checkPin(userPin) == true) {
-        userBalance = withdrawMoney(userBalance);
+    } else if (userInput == "3" && checkPin()) {
+        userBalance = withdrawMoney();
         repeatOptions();
     } else if (userInput == "4") {
-        alert("Thanks for banking with us today!");
         console.log("Thanks for banking with us today!");
+        authorized = false;
         return;
     } else {
         console.log("Input invalid");
@@ -66,15 +68,14 @@ function options() {
 }
 
 function repeatOptions() {
-    let userInput = prompt("Would you like to select another option? (yes/no)");
-    if (userInput == "yes" || userInput == "Yes") {
+    let userInput = prompt("Would you like to select another option? (y/n)");
+    if (userInput == "y" || userInput == "Y") {
         options();
-    } else if (userInput == "no" || userInput == "No") {
-        alert("Thanks for banking with us today!");
+    } else if (userInput == "n" || userInput == "N") {
         console.log("Thanks for banking with us today!");
+        authorized = false;
         return;
     } else {
-        alert("Input invalid");
         console.log("Input invalid");
         repeatOptions();
     }
@@ -82,5 +83,6 @@ function repeatOptions() {
 
 let userPin = 1234;
 let userBalance = 20;
+let authorized = false;
 
 options();
