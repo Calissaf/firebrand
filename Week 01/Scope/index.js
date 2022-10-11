2d6da2f0f4b1092e6182e27c82788a3833a37998
Calissa Fairburn <calissa@praxitar.com>
2022-10-11 11:17:57 +0100

    Initial commit for intro to variable scopes

---
+++ Week 01/Scope/index.js
@@ -0,0 +1,10 @@
+//  global variables are the variables that are accessible to evrything in the file
+// local variables are variables created within {} on a new indentation line i.e. within an if statement or a function
+
+let firstName = "Calissa";
+let age = 57;
+let location = "Ipswich";
+
+const display = () => {
+    console.log(`Hello, Iam ${firstName}. I am ${age} years old and I live in ${location}`);
+}
