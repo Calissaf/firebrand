4b5745bed1016d6d07f044e57997d898c0887f44
Calissa Fairburn <calissa@praxitar.com>
2022-10-11 11:22:04 +0100

    Initial commit for setting up an SQL database

---
+++ SQL/CreateDatabase.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE Firebrand;
+
+USE Firebrand; -- Selects the Firebrand DB to use for out queries -- we don't always need this line depends on the SQL editor you're using
+
+-- Each line/statement can be treated individually
+-- To run a query, we have a couple options:
+    -- 1. press run
+    -- 2. highlight the code you want to run, and then press run
---
+++ SQL/CreateTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Employees (
+    EmployeeID int PRIMARY KEY,
+    FullName varchar(255),
+    Company varchar(255),
+    WFH bit -- bit is SQL version of booleans (True or False)
+
+)
